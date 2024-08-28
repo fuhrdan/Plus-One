63d807c7660ac6d6abd876b8b12453a1fa0141c3
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-08-28 10:27:45 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//*****************************************************************************
+//** 66. Plus One  leetcode                                                  **
+//** Set it up to increase the size if you go from 9 to 10 on incrementing   **
+//*****************************************************************************
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int carry = 1;
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        int sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+
+    if (carry) {
+        *returnSize = digitsSize + 1;
+        int* retOne = (int*)malloc(*returnSize * sizeof(int));
+        retOne[0] = carry;
+        for (int i = 0; i < digitsSize; i++) {
+            retOne[i + 1] = digits[i];
+        }
+        return retOne;
+    } else {
+        *returnSize = digitsSize;
+        int* retOne = (int*)malloc(*returnSize * sizeof(int));
+        for (int i = 0; i < digitsSize; i++) {
+            retOne[i] = digits[i];
+        }
+        return retOne;
+    }
+}
